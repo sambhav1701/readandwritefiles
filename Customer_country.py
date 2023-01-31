67c58f2024aeeaef8d6ff3bb17117ff861382e3b
Sambhav Jain <sambhav.1701@outlook.com>
2023-01-30 23:55:31 -0600

    finished

--- Customer_country.py
+++ Customer_country.py
@@ -5,9 +5,15 @@ with open ("customers.csv", 'r') as csv_file:
     
     with open("customer_country.csv", 'w', newline = "") as new_file:
         csv_writer = csv.writer(new_file)
+
+        k = -1
 	
         for row in csv_reader:
             x = row[1] + " " + row[2], " " + row[4]
             csv_writer.writerow(x)
-
+            k += 1
+            
+print("The total number of customer read are ", k)
 new_file.close()
+
+
--- employee_bonus.py
+++ employee_bonus.py
@@ -3,5 +3,15 @@ import csv
 with open ("EmployeePay.csv", 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter = ',')
 
+    header = next(csv_reader)
+
+    print('ID','FirstName','LastName','Total salary')
+
     for row in csv_reader:
-        print(" ".join(row))
+        x = str(row[0]) + " " + str(row[1]), " " + str(row[2])
+        y = float(row[3]) * float(row[4]) + float(row[3])
+        print(f'{" ".join(x)}, {str(y)}')
+
+        Enter = input("press ENTER to continue or type STOP to end")
+        if Enter == 'STOP':
+            break
